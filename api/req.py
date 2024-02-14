1a04cb196c7ebb5e54cb1585de73785311ca7169
binhvu3 <binhhtvu@gmail.com>
2024-02-14 14:30:39 -0800

    able to get tasks back after post

--- api/req.py
+++ api/req.py
@@ -4,4 +4,6 @@ import requests
 api_url = "https://jsonplaceholder.typicode.com/todos/1"
 response = requests.get(api_url)
 
-print(f"API get response from {api_url} ---> \n {response.json()}")
+print(f"API get response {api_url} ---> {response.json()}")
+
+print(f"Reponse status {response.status_code}")
